15f316940140f9b335c429fec393b67d0acc54af
Seema Devi <160100069+Seema-Devi@users.noreply.github.com>
2024-03-15 19:40:18 +1300

    first commit

---
+++ index.test.js
@@ -0,0 +1,17 @@
+const CarValue = require('./index');
+
+test('Car value calculation for Civic 2014', () => {
+    expect(CarValue("Civic", 2014)).toBe(6614);
+});
+
+test('Car value calculation for Accord 2021', () => {
+    expect(CarValue("Accord", 2021)).toBe(6421);
+});
+
+test('Car value calculation for 911 2020', () => {
+    expect(CarValue("991", 2020)).toBe(2020);
+});
+
+test('Car value calculation for Task-Force -987', () => {
+    expect(CarValue("Task-Force", -987)).toBe("there is an error");
+});
